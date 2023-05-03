0bc13530cadb682fc2d620b4c3747b6a076a610f
leandrossb <leandroz3000@gmail.com>
2023-05-02 22:49:05 -0300

    feat: adding a factory to add fake chart infos

--- src/context/produto/index.jsx
+++ src/context/produto/index.jsx
@@ -1,14 +1,18 @@
 import React, { createContext, useContext, useState } from "react";
 
-/* 
-produto = ""
-*/
+// Using a factory to generate examples
+const dataFactory = () => {
+  return [{name: 'January', vendas: Math.floor(Math.random() * 900)},{name: 'Frebuary', vendas: Math.floor(Math.random() * 900)},
+  {name: 'March', vendas: Math.floor(Math.random() * 900)}, {name: 'April', vendas: Math.floor(Math.random() * 900)}, {name: 'May', vendas: Math.floor(Math.random() * 900)},
+  {name: 'June', vendas: Math.floor(Math.random() * 900)}, {name: 'July', vendas: Math.floor(Math.random() * 900)}, {name: 'August', vendas: Math.floor(Math.random() * 900)}]
+}
+
 
 const produtoContext = createContext([])
 export const ProdutoProvider = ({ children }) => {
   // populate produtos
   const [ produto, setProduto ] = useState(() => {
-    const local = JSON.parse(localStorage.getItem("@agile:produto")) || [{ name: "produto1", marca: "marca1"}, { name: "produto2", marca: "marca2"}, { name: "produto3", marca: "marca3"}];
+    const local = JSON.parse(localStorage.getItem("@agile:produto")) || [{ name: "produto1", marca: "marca1", data:dataFactory()}, { name: "produto2", marca: "marca2", data:dataFactory()}, { name: "produto3", marca: "marca3", data:dataFactory()}];
     return local
   })
 
