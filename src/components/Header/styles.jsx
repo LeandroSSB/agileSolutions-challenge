b003582e8cd1fa0d916965b1bcc84b367bd0780e
leandrossb <leandroz3000@gmail.com>
2023-05-02 23:07:54 -0300

    feat: adding useless header

---
+++ src/components/Header/styles.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 5vh;
+  position: fixed;
+  top: 0;
+  flex-flow: row nowrap;
+  background-color: #4294db;
+  align-items: center;
+  padding: 10px;
+  justify-content: center;
+`
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  width: 100%;
+`
+
+export const Link = styled.a`
+  text-decoration: none;
+  font-size: 20px;
+  color: white
+`
